dcba6bc4f23d9eb4c8fd9ec0bbd16615afcfb14a
Salimane Adjao Moustapha <me@salimane.com>
2016-03-26 12:41:54 +0100

    Support log4j2 log framework, needed for Storm 0.10.0
    Signed-off-by: Salimane Adjao Moustapha <salimane.moustapha@meltwater.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,13 +11,29 @@ class storm::config inherits storm {
     require => [ Package['storm'], File[$log_dir], File[$local_dir] ],
   }
 
-  file { $logback:
+  if $logback {
+    $real_log_cluster_config = $logback
+  } elsif !$log_cluster_config {
+    $real_log_cluster_config = "/opt/storm/${log_framework}/cluster.xml"
+  } else {
+    $real_log_cluster_config = $log_cluster_config
+  }
+
+  if $logback_template {
+    $real_log_template = $logback_template
+  } elsif !$log_template {
+    $real_log_template = "storm/cluster.${log_framework}.xml.erb"
+  } else {
+    $real_log_template = $log_template
+  }
+
+  file { $real_log_cluster_config :
     ensure  => file,
     owner   => root,
     group   => root,
     mode    => '0644',
-    content => template($logback_template),
+    content => template($real_log_template),
     require => File[$config],
   }
-
+  
 }
